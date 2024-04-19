6fbef35c1f7a67a8c95d60f4965630517b317b0b
Subha Bal Pal <mohit2pal@gmail.com>
2024-04-19 19:00:08 +0530

    Initial Commit

---
+++ Team-7-HMS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team-7-HMS
+//
+//  Created by Subha on 19/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Team-7-HMS/Team_7_HMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team_7_HMSApp.swift
+//  Team-7-HMS
+//
+//  Created by Subha on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Team_7_HMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
