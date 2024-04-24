38525257b28d7c5047e22b743a683b9225b069cf
Vikashini G <112184188+Vikashini-G@users.noreply.github.com>
2024-04-24 11:55:32 +0530

    Update SpecialitySwiftUIView.swift

--- Team-7-HMS/PatientHome and Appointments/SpecialitySwiftUIView.swift
+++ Team-7-HMS/PatientHome and Appointments/SpecialitySwiftUIView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct SpecialitySwiftUIView: View {
+    @State var speciality: String
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
