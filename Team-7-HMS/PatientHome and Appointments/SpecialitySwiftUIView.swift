b17b4fe7a568af26ea4e799a02a2604b3c89353b
Vikashini G <vikashini.gopinath@gmail.com>
2024-04-24 12:20:44 +0530

    add missing files patientAppointmentCard

--- Team-7-HMS/PatientHome and Appointments/SpecialitySwiftUIView.swift
+++ Team-7-HMS/PatientHome and Appointments/SpecialitySwiftUIView.swift
@@ -2,17 +2,18 @@
 //  SpecialitySwiftUIView.swift
 //  Team-7-HMS
 //
-//  Created by Vikashini G on 24/04/24.
+//  Created by Vikashini G on 23/04/24.
 //
 
 import SwiftUI
 
 struct SpecialitySwiftUIView: View {
+    @State var speciality: String
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SpecialitySwiftUIView()
+    SpecialitySwiftUIView(speciality: "ENT")
 }
