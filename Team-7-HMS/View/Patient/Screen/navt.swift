c83956715329dc0212973653b2e7ab491ce8d28c
Meghanand <ms8615@srmist.edu.in>
2024-04-28 18:57:57 +0530

    navigation bar for the patient history fixed

---
+++ Team-7-HMS/View/Patient/Screen/navt.swift
@@ -0,0 +1,23 @@
+//
+//  navt.swift
+//  Team-7-HMS
+//
+//  Created by Meghs on 28/04/24.
+//
+
+import SwiftUI
+
+struct navt: View {
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                
+            }.navigationTitle("THT")
+        }
+        
+    }
+}
+
+#Preview {
+    navt()
+}
