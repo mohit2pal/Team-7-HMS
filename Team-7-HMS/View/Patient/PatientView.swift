f4312a292341faf40e052aaf09e9c935c3b15fff
Meghanand <ms8615@srmist.edu.in>
2024-04-29 11:26:24 +0530

    navigation title in booking page fixed

--- Team-7-HMS/View/Patient/PatientView.swift
+++ Team-7-HMS/View/Patient/PatientView.swift
@@ -22,7 +22,10 @@ struct PatientView: View {
                 .tabItem {
                     Label("", systemImage: "sos")
                 }
+            
+
             BookingView(patientID: patientUid)
+            
                 .tabItem {
                     Label("", systemImage: "calendar.badge.plus")
                 }
@@ -35,5 +38,5 @@ struct PatientView: View {
 }
 
 #Preview {
-    PatientView(patientName: "Patient Name", showPatientHistory: true, patientUid: "hi")
+    PatientView(patientName: "Patient Name", showPatientHistory: true, patientUid: "Vzo9cLiS9fZTyzpzkeH0Vure5YP2")
 }
