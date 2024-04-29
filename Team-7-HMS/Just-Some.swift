c5f409314f4a26377ad194d87130050968a03670
Subha Bal Pal <mohit2pal@gmail.com>
2024-04-29 10:56:15 +0530

    Added Scroll View in The Add Doctors and phonenumber validation in add doctor

--- Team-7-HMS/Just-Some.swift
+++ Team-7-HMS/Just-Some.swift
@@ -1,40 +1,24 @@
 //import SwiftUI
 //
-//struct TabViewWithToolbar: View {
-//    var body: some View {
-//        TabView {
-//            NavigationView {
-//                Text("Home Tab")
-//                    .navigationTitle("Home")
-//                    .toolbar {
-//                        ToolbarItem(placement: .navigationBarTrailing) {
-//                            Button(action: {
-//                                print("Action button tapped")
-//                            }) {
-//                                Image(systemName: "plus")
-//                            }
-//                        }
-//                    }
-//            }
-//            .tabItem {
-//                Image(systemName: "house.fill")
-//                Text("Home")
-//            }
-//            
-//            Text("Settings Tab")
-//                .tabItem {
-//                    Image(systemName: "gear")
-//                    Text("Settings")
-//                }
-//        }
-//    }
-//}
+//struct ContentView: View {
+//    @State private var phoneNumber = ""
+//    @State private var isValidPhoneNumber = true
 //
-//@main
-//struct MyApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            TabViewWithToolbar()
-//        }
+//    var body: some View {
+//        TextField("Phone Number", text: $phoneNumber)
+//            .keyboardType(.namePhonePad)
+//            .textFieldStyle(.plain)
+//            .padding()
+//            .background(isValidPhoneNumber ? Color.white : Color.red)
+//            .cornerRadius(10)
+//            .onChange(of: phoneNumber) { newValue in
+//                isValidPhoneNumber = isValidPhone(testStr: newValue)
+//            }
+//    }
+//
+//    func isValidPhone(testStr:String) -> Bool {
+//        let phoneRegEx = "^\\d{3}-\\d{3}-\\d{4}$"
+//        let phoneTest = NSPredicate(format:"SELF MATCHES %@", phoneRegEx)
+//        return phoneTest.evaluate(with: testStr)
 //    }
 //}
