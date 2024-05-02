66a4dcb27de5cde52e86df208c7128102a6afa3d
Subha Bal Pal <mohit2pal@gmail.com>
2024-05-02 16:18:36 +0530

    Added major changes to the AddingSlot page and added validation and made the booking animation

--- Team-7-HMS/View/Admin/Screen/AddingSlots.swift
+++ Team-7-HMS/View/Admin/Screen/AddingSlots.swift
@@ -9,24 +9,56 @@ import SwiftUI
 
 struct AddingSlots: View {
     @State private var doctors: [DoctorInfo] = []
-    @State private var selectedDoctor: DoctorInfo? = nil
-    @State private var selectedDate = Date()
+    @State private var selectedDoctor: DoctorInfo? = nil {
+        didSet {
+            fetchBookedSlots()
+        }
+    }
+    @State private var selectedDate: Date? = nil {
+        didSet{
+            fetchBookedSlots()
+        }
+    }
     @State private var searchText = ""
-    @State private var showList = false
-    let firebaseHelper = FirebaseHelperFunctions()
+    @State private var bookedSlots: [String] = []
     @State private var selectedSlots: [String] = []
+    @State private var showSuccessAnimation = false // New state variable for showing the tick animation
+    
+    let firebaseHelper = FirebaseHelperFunctions()
+    
+    // Add a computed property to check if the submit button should be enabled
+    private var isSubmitButtonDisabled: Bool {
+        selectedDoctor == nil || selectedSlots.isEmpty
+    }
+    
+    private func fetchBookedSlots() {
+        guard let selectedDoctor = selectedDoctor else { return }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd_MM_yyyy"
+        let dateString = dateFormatter.string(from: selectedDate ?? Date())
+        
+        firebaseHelper.fetchSlots(doctorID: selectedDoctor.id, date: dateString) {result, error in
+            if let error = error {
+                print("Error fetching slots: \(error.localizedDescription)")
+                self.bookedSlots = []
+            } else if let result = result {
+                // Assuming the result is a dictionary with date as key and an array of slots as value
+                self.bookedSlots = result[dateString]?.compactMap { $0.keys.first } ?? []
+            }
+        }
+    }
     
     private var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = "EE\nd"
         return formatter
     }
-        
-        private var daysOfWeek: [Date] {
-            let calendar = Calendar.current
-            let today = calendar.startOfDay(for: Date())
-            return (0..<7).map { calendar.date(byAdding: .day, value: $0, to: today)! }
-        }
+    
+    private var daysOfWeek: [Date] {
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: Date())
+        return (0..<7).map { calendar.date(byAdding: .day, value: $0, to: today)! }
+    }
     
     // Generate time slots from 9 am to 4 pm
     let timeSlots: [String] = {
@@ -45,150 +77,20 @@ struct AddingSlots: View {
     
     var body: some View {
         VStack {
-//            ScrollView(.horizontal) {
-//                HStack {
-//                    ForEach(doctors, id: \.id) { doctor in
-//                        VStack(alignment: .leading) {
-//                            Text("Name: \(doctor.name)")
-//                            Text("Specialty: \(doctor.specialty)")
-//                        }
-//                        .padding()
-//                        .background(selectedDoctor?.id == doctor.id ? Color.blue.opacity(0.5) : Color.clear)
-//                        .cornerRadius(10)
-//                        .onTapGesture {
-//                            // Set the selected doctor
-//                            selectedDoctor = doctor
-//                            // Clear the selected slots when the doctor changes
-//                            selectedSlots = []
-//                        }
-//                    }
-//                }
-//            }
-            
-            HStack{
-                Text("Select Doctor")
-                    .font(.headline)
-                Spacer()
+            if selectedDoctor == nil {
+                // Doctor selection view
+                doctorSelectionView
+                    .transition(.move(edge: .leading))
+            } else {
+                // Date and slot selection view
+                dateAndSlotSelectionView
+                    .transition(.move(edge: .trailing))
             }
-            VStack {
-                SearchBar(text: $searchText, showList: $showList)
-                if showList {
-                    List {
-                        ForEach(doctors.filter {
-                            self.searchText.isEmpty ? true : $0.name.localizedCaseInsensitiveContains(searchText)
-                        }, id: \.name) { doctor in
-                            Button(action: {
-                                self.selectedDoctor = doctor
-                                self.showList = false
-                            }) {
-                                Text("\(doctor.name), \(doctor.specialty)")
-                            }
-                        }
-                    }
-                    .listStyle(.plain)
-                    .background(Color.white)
-                    .cornerRadius(20)
-                    .customShadow()
-                }
-                
-                // Date picker
-                Spacer().frame(height: 30)
-                HStack{
-                    Text("Select Date")
-                        .font(.headline)
-                    Spacer()
-    //                DatePicker("", selection: $selectedDate, displayedComponents: .date)
-    //                    .padding()
-                }
-                
-                //DATES SCROLL
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 20) {
-                        ForEach(daysOfWeek, id: \.self) { date in
-                            DateButton(date: date, isSelected: date == selectedDate, action: {
-                                selectedDate = date
-                            })
-                        }
-                    }
-                }
-                Spacer().frame(height: 30)
-//                if let selectedDoctor = selectedDoctor {
-//                    HStack{
-//                        Text("Selected Doctor:")
-//                            .font(.headline)
-//                        Spacer()
-//                        Text("\(selectedDoctor.name)")
-//                    }
-//                    
-//                }
-            }
-            .background(
-                Color.clear
-                    .contentShape(Rectangle()) 
-                    // Make the background tappable
-                    .onTapGesture {self.showList = false}
-                    // Hide the list when tapped outside the search bar or the list
-            )
-            
-            
-            
-            // Display time slots when a doctor is selected
-            if let selectedDoctor = selectedDoctor {
-                HStack{
-                    Text("Set time slots for")
-                    Text("\(selectedDoctor.name)")
-                        .foregroundStyle(.gray)
-                    Spacer()
-                }
-                .font(.headline)
-                .padding(.vertical)
-            
-                
-                // time slots
-                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 20) {
-                    ForEach(timeSlots, id: \.self) { slot in
-                        Button(action: {
-                            // Toggle the selection of the time slot
-                            if selectedSlots.contains(slot) {
-                                selectedSlots.removeAll(where: { $0 == slot })
-                            } else {
-                                selectedSlots.append(slot)
-                            }
-                        }) {
-                            Text(slot)
-                                .padding(.horizontal)
-                                .padding(.vertical, 10)
-                                .background(selectedSlots.contains(slot) ? Color.myAccent : Color.white)
-                                .foregroundStyle(selectedSlots.contains(slot) ? Color.white : Color.black)
-                                .cornerRadius(8)
-                                .customShadow()
-                        }
-                        .frame(width: 180)
-                    }
-                }
-                .padding(.horizontal)
-            }
-            Spacer()
-            Button(action: {
-                // Print details of the selected doctor
-                if let selectedDoctor = selectedDoctor {
-                    firebaseHelper.createSlots(doctorName: selectedDoctor.name, doctorID: selectedDoctor.id, date: selectedDate, slots: selectedSlots)
-                } else {
-                    print("No doctor selected.")
-                }
-            }, label: {
-                Text("Press here to submit")
-            })
-//            .padding()
-            .frame(width: 296, height: 44)
-            .background(Color.myAccent)
-            .foregroundColor(.white)
-            .cornerRadius(20)
         }
         .padding()
         .background(Color.background)
+        .animation(.easeInOut, value: selectedDoctor)
         .onAppear {
-            // Fetch the list of doctors when the view appears
             firebaseHelper.fetchAllDoctors { result in
                 switch result {
                 case .success(let doctors):
@@ -199,17 +101,166 @@ struct AddingSlots: View {
             }
         }
     }
+    
+    // MARK: - Doctor Selection View
+    var doctorSelectionView: some View {
+        VStack {
+            HStack {
+                Text("Select Doctor")
+                    .font(.headline)
+                Spacer()
+            }
+            SearchBar(text: $searchText)
+                .padding(.bottom)
+            List {
+                ForEach(doctors.filter {
+                    self.searchText.isEmpty ? true : $0.name.localizedCaseInsensitiveContains(searchText)
+                }, id: \.name) { doctor in
+                    Button(action: {
+                        self.selectedDoctor = doctor
+                    }) {
+                        Text("\(doctor.name), \(doctor.specialty)")
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .background(Color.white)
+            .cornerRadius(20)
+            .customShadow()
+        }
+    }
+    
+    // MARK: - Date and Slot Selection View
+    var dateAndSlotSelectionView: some View {
+        VStack {
+            // Include a back button at the top
+            HStack {
+                Button(action: {
+                    // Reset selectedDoctor to nil to go back
+                    self.selectedDoctor = nil
+                    self.selectedDate = nil
+                    self.selectedSlots = []
+                }) {
+                    Image(systemName: "arrow.left") // Use a suitable image or text
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+                if let doctorName = selectedDoctor?.name {
+                    Text(doctorName)
+                        .font(.headline)
+                        .padding()
+                }
+                Spacer()
+            }
+            
+            // Date picker
+            HStack {
+                Text("Select Date")
+                    .font(.headline)
+                Spacer()
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(daysOfWeek, id: \.self) { date in
+                        DateButton(date: date, isSelected: date == selectedDate, action: {
+                            selectedDate = date
+                        })
+                    }
+                }
+            }
+            .padding(.vertical)
+            
+            // Time slots
+            if let selectedDoctor = selectedDoctor, selectedDate != nil {
+                HStack {
+                    Text("Set time slots for")
+                    Text("\(selectedDoctor.name)")
+                        .foregroundStyle(.gray)
+                    Spacer()
+                }
+                .font(.headline)
+                .padding(.vertical)
+                
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 20) {
+                    ForEach(timeSlots, id: \.self) { slot in
+                        Button(action: {
+                            // Check if the slot is not booked before toggling the selection
+                            if !bookedSlots.contains(slot) {
+                                if selectedSlots.contains(slot) {
+                                    selectedSlots.removeAll(where: { $0 == slot })
+                                } else {
+                                    selectedSlots.append(slot)
+                                }
+                            }
+                        }) {
+                            Text(slot)
+                                .padding()
+                                .background(bookedSlots.contains(slot) ? Color.gray : (selectedSlots.contains(slot) ? Color.myAccent : Color.white))
+                                .foregroundStyle(bookedSlots.contains(slot) ? Color.white : (selectedSlots.contains(slot) ? Color.white : Color.black))
+                                .cornerRadius(8)
+                                .customShadow()
+                                .disabled(bookedSlots.contains(slot))
+                        }
+                    }
+                }
+            }
+            
+            Spacer()
+            submitButton
+        }
+    }
+    
+    
+    // MARK: - Submit Button
+    var submitButton: some View {
+        Button(action: {
+            if let selectedDoctor = selectedDoctor {
+                firebaseHelper.createSlots(doctorName: selectedDoctor.name, doctorID: selectedDoctor.id, date: selectedDate ?? Date(), slots: selectedSlots)
+                self.showSuccessAnimation = true
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    self.showSuccessAnimation = false
+                    self.selectedDoctor = nil
+                    self.selectedDate = nil
+                    self.selectedSlots = []
+                }
+            } else {
+                print("No doctor selected.")
+            }
+        }, label: {
+            Text("Press here to submit")
+        })
+        .frame(width: 296, height: 44)
+        .background(isSubmitButtonDisabled ? Color.gray : Color.myAccent)
+        .foregroundColor(.white)
+        .cornerRadius(20)
+        .disabled(isSubmitButtonDisabled)
+        .fullScreenCover(isPresented: $showSuccessAnimation) {
+            // FullScreenCover for success animation
+            SuccessAnimationView()
+        }
+    }
+}
+
+struct SuccessAnimationView: View {
+    var body: some View {
+        ZStack {
+            Color.background.edgesIgnoringSafeArea(.all)
+            Image(systemName: "checkmark.circle.fill") // Use a tick mark image or system symbol
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .foregroundColor(.green)
+        }
+    }
 }
 
 struct SearchBar: View {
     @Binding var text: String
-    @Binding var showList: Bool
     
     var body: some View {
         HStack {
-            TextField("Search doctors by name", text: $text, onEditingChanged: { editing in
-                self.showList = editing
-            })
+            TextField("Search doctors by name", text: $text)
                 .padding(8)
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
