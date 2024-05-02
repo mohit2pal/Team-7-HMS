5180e23951285080e62e6a1d5e768943401733c5
Meghanand <ms8615@srmist.edu.in>
2024-05-02 16:16:23 +0530

    cancel appointments

---
+++ Team-7-HMS/View/Patient/Screen/AppointmentDataView.swift
@@ -0,0 +1,18 @@
+//
+//  AppointmentDataView.swift
+//  Team-7-HMS
+//
+//  Created by Meghs on 02/05/24.
+//
+
+import SwiftUI
+
+struct AppointmentDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppointmentDataView()
+}
