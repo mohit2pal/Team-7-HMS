cb4229f303ee5530edcc8b082b30046e5136de5a
ektakalra <kalraekta6@gmail.com>
2024-05-03 14:33:56 +0530

    core data lab

---
+++ Team-7-HMS/Model/LabTestCardData.swift
@@ -0,0 +1,25 @@
+//
+//  LabTestCardData.swift
+//  Team-7-HMS
+//
+//  Created by Ekta on 03/05/24.
+//
+
+import SwiftUI
+
+struct LabTestCardData: Identifiable {
+  let id = UUID()
+  let date: String
+  let day: String
+  let testName: String
+  let doctorName: String
+}
+
+struct LabTestMockData {
+  static let labTestDataArray = [
+    LabTestCardData(date: "11", day: "Mon", testName: "Blood Test", doctorName: AppointmentMockData.appointmentDataArray[0].doctorName),
+    LabTestCardData(date: "12", day: "Tue", testName: "Urine Test", doctorName: AppointmentMockData.appointmentDataArray[1].doctorName),
+    LabTestCardData(date: "10", day: "Sun", testName: "X-Ray", doctorName: "Dr. X-Ray"),
+    LabTestCardData(date: "13", day: "Wed", testName: "MRI", doctorName: "Dr. Scan"),
+  ]
+}
---
+++ Team-7-HMS/PatientHome and Appointments/LabReports/ViewLabReports.swift
@@ -0,0 +1,20 @@
+//
+//  ViewLabReports.swift
+//  Team-7-HMS
+//
+//  Created by Ekta  on 03/05/24.
+//
+
+import SwiftUI
+
+struct ViewLabReports: View {
+    @State var appointmentData: AppointmentCardData
+var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    
+}
+
