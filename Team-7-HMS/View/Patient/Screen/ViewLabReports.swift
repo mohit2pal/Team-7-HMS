e90ad2157e06d99a355714078ec03f87bcbef58e
ektakalra <kalraekta6@gmail.com>
2024-05-03 10:16:44 +0530

    View lab reports page

---
+++ Team-7-HMS/View/Patient/Screen/ViewLabReports.swift
@@ -0,0 +1,18 @@
+//
+//  ViewLabReports.swift
+//  Team-7-HMS
+//
+//  Created by Ekta  on 02/05/24.
+//
+
+import SwiftUI
+
+struct ViewLabReports: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewLabReports()
+}
