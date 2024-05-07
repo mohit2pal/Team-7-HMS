7016244972a1ae1c5d228579616c6639d3da4e26
ektakalra <kalraekta6@gmail.com>
2024-05-07 11:53:55 +0530

    mock data for leave approval

--- Team-7-HMS/PatientHome and Appointments/LabReports/LeaveCardData.swift
+++ Team-7-HMS/PatientHome and Appointments/LabReports/LeaveCardData.swift
@@ -8,11 +8,19 @@
 import Foundation
 
 struct LeaveCardData: Identifiable {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            var id = UUID()
+            var date: String
+            var year: Int
+            var day: String
+            var DoctorName: String
+            var status: String
+            var DoctorID : String
     }
-}
-
-#Preview {
-    LeaveCardData()
+struct LeaveMockData {
+    static let LeaveDataArray = [
+        LeaveCardData(date: "14 Apr", year: 24, day: "Wed",DoctorName:"Dr. Fog" ,status: "Approved",DoctorID: ""),
+        LeaveCardData(date: "16 Apr", year: 24, day: "Fri",DoctorName:"Dr. Aditya",status: "Pending", DoctorID: ""),
+        LeaveCardData(date: "17 Apr", year: 24, day: "Sat",DoctorName:"Dr. Fog",status: "Approved",DoctorID: ""),
+        LeaveCardData(date: "19 Apr", year: 24, day: "Mon",DoctorName:"Dr. Ekta",status: "Pending",DoctorID: "")
+        ]
 }
