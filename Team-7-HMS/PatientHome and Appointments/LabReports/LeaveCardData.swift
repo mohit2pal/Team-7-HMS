f4f53247c60d5a843f1b6496388e983518a6ecec
ektakalra <kalraekta6@gmail.com>
2024-05-07 11:49:18 +0530

    mock data for leave

---
+++ Team-7-HMS/PatientHome and Appointments/LabReports/LeaveCardData.swift
@@ -0,0 +1,18 @@
+//
+//  LeaveApprovalCardData.swift
+//  Team-7-HMS
+//
+//  Created by Ekta  on 07/05/24.
+//
+
+import Foundation
+
+struct LeaveCardData: Identifiable {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeaveCardData()
+}
